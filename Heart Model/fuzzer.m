6b460285770ffa427aa994e07c5cf4379db63c57
Aron Harder <aron.harder@gmail.com>
2019-05-03 10:46:16 -0400

    Retrieve output

--- Heart Model/fuzzer.m
+++ Heart Model/fuzzer.m
@@ -2,15 +2,14 @@
 %https://www.mathworks.com/help/simulink/ug/using-the-sim-command.html
 %Accessing data: https://www.mathworks.com/matlabcentral/answers/384492-how-to-access-scope-data-when-running-model-with-sim-command
 
-Cf_Sweep= linspace(0,30);
-Cf_Sweep2= linspace(0,30);
+Cf_Sweep= linspace(0,30,5);
+Cf_Sweep2= linspace(0,30,5);
 numExperiments= length(Cf_Sweep);
-
 
 Cf_Sweep= Cf_Sweep(randperm(length(Cf_Sweep)));
 Cf_Sweep2= Cf_Sweep2(randperm(length(Cf_Sweep2)));
 
-data=[];
+%data=[];
 for i= numExperiments:-1:1
     in(i)=Simulink.SimulationInput('NPNwithVVI'); %name of project
     in(i)=in(i).setBlockParameter('NPNwithVVI/NodeLongERP1/Rest_def','Value',num2str(Cf_Sweep(i)));
@@ -23,3 +22,18 @@ for i= numExperiments:-1:1
 end
 
 out= parsim(in);
+
+NodeLongERP = out(1).logsout{1}.Values;
+NodeLongERP1 = out(1).logsout{2}.Values;
+
+%NodeLongERP.data
+
+% variable1 = ( beats in interval / interval length )
+% convert variable1 to beats/minute
+
+%for i=out
+%  i.logsout
+%end
+
+%Simulink.sdi.view;
+
